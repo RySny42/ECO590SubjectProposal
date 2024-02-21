005459dab46bc63907bc17838bbd0879c5debaae
RySny42 <157412015+RySny42@users.noreply.github.com>
2024-02-20 20:23:52 -0500

    Test

---
+++ Proposal Test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#“If Americans want to live the American dream, they should go to Denmark.”-Richard G. Wilkinson, Social Epidemiologist at The University of Nottingham
+
+
+# In[3]:
+
+
+#If a parent's income had no effect on a child's opportunity for future upward mobility, approximately 20% of poor children who started in the bottom quintile (in the bottom 20% of the US range of incomes) would remain there as poor adults. At the other end of income spectrum, if children were born into wealthy families in the top 20%, only 20% would stay in that top income category if their mobility opportunities were equal to every other child's in the country.
+
+
+# In[5]:
+
+
+from IPython.display import Image
+Image(url='https://bpb-eu-w2.wpmucdn.com/blogs.ucl.ac.uk/dist/5/694/files/2016/03/graph11.jpg')
+
+
+# In[ ]:
+
+
+
+
