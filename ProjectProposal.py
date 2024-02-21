44a01432e5186684b9535ea09174f7706722becd
RySny42 <157412015+RySny42@users.noreply.github.com>
2024-02-20 19:52:31 -0500

    Add files via upload

---
+++ ProjectProposal.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#“If Americans want to live the American dream, they should go to Denmark.”-Richard G. Wilkinson, Social Epidemiologist at The University of Nottingham
+
+
+# In[ ]:
+
+
+
+
